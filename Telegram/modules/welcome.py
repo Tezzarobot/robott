f68c0c61b9c08d1b79b9ddd62f128bf9c5866dfc
TEZZA <93602145+Tezzarobot@users.noreply.github.com>
2022-06-24 11:27:31 +0530

    Update welcome.py

--- Telegram/modules/welcome.py
+++ Telegram/modules/welcome.py
@@ -229,7 +229,7 @@ def new_member(update: Update, context: CallbackContext):  # sourcery no-metrics
             # Welcome Devs
             elif new_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "Whoa! A member of the Anie just joined!",
+                    "Whoa! A member of the Tezza just joined!",
                     reply_to_message_id=reply,
                 )
                 continue
